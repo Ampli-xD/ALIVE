a992f1f88897ae607072c1635e59aac470558956
Dishant Kapoor <dishantkapoor29@gmail.com>
2021-08-02 05:33:08 +0000

    IT WORKS NOW :D

--- bot.py
+++
@@ -1,4 +0,0 @@
-from discord.ext import commands
-
-bot = commands.Bot(command_prefix='', self_bot=True)
-bot.remove_command(help)
--- main.py
+++ main.py
@@ -2,20 +2,34 @@ import os
 import discord
 from discord.ext import commands
 from webserver import keep_alive as keep_alive
-from bot import bot
 
-Token = os.environ['Token']
+ClientToken = os.environ['MyClientToken']
 
+intents = discord.Intents.default()
+intents.members = True
+MyClient = commands.Bot(command_prefix='!', self_bot=True, intents=intents)
+MyClient.remove_command(help)
 
-@bot.event
+@MyClient.event
 async def on_ready():
-  print(bot.user.name)
+  print(MyClient.user.name)
+  await MyClient.change_presence(activity=discord.Activity(type=3, name="You Smarty Pants      oooooooooooooooooooo"), status=discord.Status.online)
 
-  await bot.change_presence(activity=discord.Activity(type=3, name="You Smarty Pants      oooooooooooooooooooo"), status=discord.Status.online)
 
-@bot.event
-async def on_command_error(ctx, error):
-  pass
+
+@MyClient.event
+async def on_message(message):
+    if message.author!=MyClient.user:
+        async for message in message.channel.history(limit=1):
+          user = MyClient.get_user(851116367053717545)
+          if "Direct" in str(message.channel):
+            await user.send(f"""
+            **DM {message.author}:** {message.content}""")
+          else:
+            await user.send(f"""**{message.guild}**
+            **{message.channel}**
+            **{message.author}:** *{message.content}*""")
+    
 
 keep_alive()
-bot.run(Token, bot=False)
+MyClient.run(ClientToken, bot=False)
